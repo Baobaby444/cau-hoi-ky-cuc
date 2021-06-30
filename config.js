b277f79d3e364f392f38b14955c987fedb99a8db
Baobaby444 <86722382+Baobaby444@users.noreply.github.com>
2021-06-30 19:11:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Baeee à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Phương cứ đi nhầm đường
+    Lạc vào tim Bảo lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: 'hihi'
+    title: 'Trả lời câu hỏi này vội cho Bảo 😜',
+    desc: 'Phương đã phần nào đó có ý với Bảo rồi phải không ',
+    btnYes: 'Đúng luôn♥️',
+    btnNo: 'chả thích tẹo nào 🙄',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Phương lại thích Bảo <3',
+    btnReply: 'Gửi cho Bảo <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'awwwwww',
+    messDesc: 'đúng yêu là phải nói mà.',
+    btnAccept: 'dzaaaaaaa <3',
+    messLink: 'http://fb.com' https://www.facebook.com/messages/t/100047513075387 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
